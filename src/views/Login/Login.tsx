b3f15fac1bcd473b2aa3bddb4a56ee4d7b668abe
Vinícius Smanioto <smaniotov@gmail.com>
2020-03-31 02:47:34 -0300

    feat: 🎸 add styles for login and improve login page

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -3,14 +3,19 @@ import { useCustomFormik } from 'hooks'
 import React from 'react'
 import { FormattedMessage, useIntl } from 'react-intl'
 import { Link } from 'react-router-dom'
+import { Button, Container, Form, Grid, Image, Input } from 'semantic-ui-react'
 import {
-  Button,
-  Checkbox,
-  Container,
-  Form,
-  Grid,
-  Input,
-} from 'semantic-ui-react'
+  HeadingContainer,
+  LoginContainer,
+  StyledLoginSubtitle,
+  StyledLoginTitle,
+  ButtonContainer,
+  LogoContainer,
+  StyledForgotPassword,
+} from './styles'
+
+// eslint-disable-next-line global-require
+const logoIcon = require('assets/icons/logo.svg') as string
 
 const Login = () => {
   const intl = useIntl()
@@ -18,66 +23,81 @@ const Login = () => {
     initialValues: {
       username: '',
       password: '',
-      terms: false,
     },
-    onSubmit: ({ username, password, terms }) => {
-      alert(
-        `Username: ${username}
-        Password: ${password}
-        Terms: ${terms}`
-      )
-    },
+    onSubmit: console.log,
   })
 
   return (
-    <Container>
-      <Grid>
-        <Grid.Row style={{ display: 'flex', justifyContent: 'center' }}>
-          <Grid.Column width={5}>
-            <div style={{ marginTop: '90px' }}>
-              <h3>
-                <FormattedMessage id="login.title" />
-              </h3>
-              <div style={{ marginTop: '10px' }}>
-                <Form onSubmit={formik.handleSubmit}>
-                  <Form.Field
-                    {...createProps('username')}
-                    type="text"
-                    placeholder={intl.formatMessage({
-                      id: 'form.placeholder.username',
-                    })}
-                    label={intl.formatMessage({ id: 'form.label.username' })}
-                    control={Input}
-                  />
-                  <Form.Field
-                    control={Input}
-                    {...createProps('password')}
-                    type="password"
-                    placeholder={intl.formatMessage({
-                      id: 'form.placeholder.password',
-                    })}
-                    label={intl.formatMessage({ id: 'form.label.password' })}
-                  />
-                  <Form.Field
-                    {...createProps('terms')}
-                    label="I agree to the Terms and Conditions"
-                    control={Checkbox}
-                  />
-                  <div style={{ marginBottom: 10 }}>
-                    <Link to={RouteEnum.Register}>
-                      <FormattedMessage id="login.signUp" />
-                    </Link>
-                  </div>
-                  <Button type="submit" disabled={!formik.values.terms}>
-                    <FormattedMessage id="form.submit" />
-                  </Button>
-                </Form>
+    <LoginContainer>
+      <Container>
+        <Grid>
+          <Grid.Row style={{ display: 'flex', justifyContent: 'center' }}>
+            <Grid.Column largeScreen={4} mobile={12} tablet={8}>
+              <div>
+                <LogoContainer>
+                  <Image src={logoIcon} />
+                </LogoContainer>
+                <HeadingContainer>
+                  <StyledLoginTitle>
+                    <FormattedMessage id="login.title" />
+                  </StyledLoginTitle>
+                  <StyledLoginSubtitle>
+                    <FormattedMessage id="login.subtitle" />
+                  </StyledLoginSubtitle>
+                </HeadingContainer>
+                <div>
+                  <Form onSubmit={formik.handleSubmit}>
+                    <Form.Field
+                      {...createProps('username')}
+                      type="text"
+                      placeholder={intl.formatMessage({
+                        id: 'form.placeholder.username',
+                      })}
+                      label={intl.formatMessage({ id: 'form.label.username' })}
+                      control={Input}
+                    />
+                    <Form.Field>
+                      <label htmlFor="password">
+                        <FormattedMessage id="form.label.password" />
+                        <StyledForgotPassword>
+                          <Link to={RouteEnum.ForgotPassword}>
+                            <FormattedMessage id="login.forgotPassword" />
+                          </Link>
+                        </StyledForgotPassword>
+                      </label>
+
+                      <Input
+                        {...createProps('password')}
+                        type="password"
+                        placeholder={intl.formatMessage({
+                          id: 'form.placeholder.password',
+                        })}
+                      />
+                    </Form.Field>
+                    <ButtonContainer>
+                      <Grid>
+                        <Grid.Column width={1} />
+                        <Grid.Column width={10} textAlign="center">
+                          <Button fluid type="submit" color={'primary' as any}>
+                            <FormattedMessage id="form.login" />
+                          </Button>
+                        </Grid.Column>
+                      </Grid>
+                    </ButtonContainer>
+                    <div style={{ textAlign: 'center' }}>
+                      <FormattedMessage id="login.notSignedYet" />
+                      <Link to={RouteEnum.Register}>
+                        <FormattedMessage id="login.registerHere" />
+                      </Link>
+                    </div>
+                  </Form>
+                </div>
               </div>
-            </div>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Container>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </LoginContainer>
   )
 }
 
---
+++ src/views/Login/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const LoginContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const StyledLoginTitle = styled.h1`
+  margin-bottom: 10px;
+  color: #12263f;
+`
+
+export const StyledLoginSubtitle = styled.span`
+  color: #adadad;
+  margin-top: 0;
+  font-size: 18px;
+  font-weight: 200;
+`
+
+export const HeadingContainer = styled.div`
+  margin-top: 32px;
+  margin-bottom: 42px;
+  text-align: center;
+`
+
+export const ButtonContainer = styled.div`
+  margin-top: 42px;
+  margin-bottom: 20px;
+`
+
+export const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+export const StyledForgotPassword = styled.span`
+  float: right;
+  a {
+    color: #adadad;
+  }
+`
